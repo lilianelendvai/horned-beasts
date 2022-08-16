c38f71c1d31ded873ed1fba61e5bdc964536d039
Liliane Lendvai <lilianelendvai@gmail.com>
2022-08-15 23:50:43 -0700

    initial lab 1 attempt

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+
+  constructor( props ) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>a beast</h2>
+      </div>
+    );
+  }
+
+}
+
+export default HornedBeast;
