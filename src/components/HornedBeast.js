ad7c864a8b773fde24c9daa75e92e21a158046c3
Liliane Lendvai <lilianelendvai@gmail.com>
2022-08-16 23:38:40 -0700

    updated with lab 2 requirements

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -2,12 +2,27 @@ import React from 'react';
 
 class HornedBeast extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment = (e) => {
+    let nextState = {
+      count: this.state.count + 1
+    }
+    this.setState( nextState );
+  }
+
   render() {
     return (
       <div>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt="A horned beast" title={this.props.title} width="200"/>
+        <img src={this.props.imageUrl} alt="A horned beast" title={this.props.title} width="200" onClick = {this.increment} />
         <p>{this.props.description}</p>
+        <p>❤️ {this.state.count}</p>
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
 
+import data from '../data.json';
+
 class Main extends React.Component {
 
   render() {
     return (
-      <div>
-        <HornedBeast description="beast one" imageUrl="https://images.unsplash.com/photo-1577493340887-b7bfff550145?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=640&q=80" title="Harry Potter Dragon"/>
-        <HornedBeast description="beast two" imageUrl="https://images.unsplash.com/photo-1550747528-cdb45925b3f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80" title="Lego Unikitty"/>
-      </div>
+      <>
+        {
+          data.map( (value, index) => (
+            <HornedBeast description={value.description} imageUrl={value.image_url} title={value.title} /> 
+          ))
+        }
+
+      </>
     );
   }
 
